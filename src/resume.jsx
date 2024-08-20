d4093fb386f4a6043b0c9f5153c2b70b321b3744
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2024-08-19 21:14:16 -0700

    style: update aesthetic

--- src/resume.jsx
+++ src/resume.jsx
@@ -1,9 +1,9 @@
 import { Fragment } from "react";
-import EducationCard from './components/education/education-card';
+import SectionCard from './components/section-card';
 import PropTypes from 'prop-types'
 
 
-export default function Resume ({ person, education, sections}) {
+export default function Resume ({ person, sections}) {
 
     return (
         <Fragment>
@@ -15,31 +15,29 @@ export default function Resume ({ person, education, sections}) {
                     <p className="resume-address">{person.address}</p>
                 </div>
                 <div className="resume-details">
-                    <div>
-                        <h3 className="details-title">Education</h3>
-                        <EducationCard school={education.currentForm} />
+                <div>
+                    <h3 className="details-title">Education</h3>
+                    <br></br>
+                    {sections.education.schools.map(school => (
+                        <>
+                        <SectionCard section={school}/>
                         <br></br>
-                        {education.schools.map((school) => (
-                            <>
-                            <EducationCard key={school.id} school={school} />
-                            <br></br>
-                            </>
-                        ))}
-                    </div>
+                        </>
+                    ))}
+                        <div>
+                         <SectionCard section={sections.education.currentForm}/>
+                        </div>
+                   </div>
                    <div>
-                    <h3 className="details-title">Experience</h3>
+                    <h3 className="details-title">Professional Experience</h3>
                     <br></br>
                     {sections.career.jobs.map(job => (
                         <>
-                        <div key={job.id}>
-                            <p>{job.company}</p>
-                            <p>{job.position}</p>
-                            <p>{job.description}</p>
-                            <p>{job.start}</p>
-                            <p>{job.end}</p>
-                        </div>
+                        <SectionCard section={job}/>
+                        <br></br>
                         </>
                     ))}
+                        <SectionCard section={sections.career.currentForm}/>
                    </div>
                 </div>
             </div>
@@ -55,23 +53,27 @@ Resume.propTypes = {
         phone: PropTypes.string.isRequired,
         address: PropTypes.string.isRequired
     }).isRequired,
-    education: PropTypes.shape({
-        schools: PropTypes.arrayOf(
-            PropTypes.shape({
-                id: PropTypes.string.isRequired,
-                school: PropTypes.string.isRequired,
-                degree: PropTypes.string.isRequired,
-                location: PropTypes.string.isRequired,
-                start: PropTypes.string.isRequired,
-                end: PropTypes.string.isRequired,
+    sections: PropTypes.shape({
+        education: PropTypes.shape({
+            schools: PropTypes.arrayOf(
+                PropTypes.shape({
+                    [PropTypes.string]: PropTypes.string.isRequired,
+                }),
+            ),
+            currentForm: PropTypes.shape({
+                [PropTypes.string] : PropTypes.string.isRequired
+            })    
+            
+        }),
+        career: PropTypes.shape({
+            jobs: PropTypes.arrayOf(
+                PropTypes.shape({
+                [PropTypes.string]: PropTypes.string.isRequired,
+                })
+            ),
+            currentForm: PropTypes.shape({
+                [PropTypes]: PropTypes.string.isRequired,
             })
-        ).isRequired,
-        currentForm: PropTypes.shape({
-            school: PropTypes.string.isRequired,
-            degree: PropTypes.string.isRequired,
-            location: PropTypes.string.isRequired,
-            start: PropTypes.string.isRequired,
-            end: PropTypes.string.isRequired,
-        }).isRequired
+        })
     }).isRequired,
 }
